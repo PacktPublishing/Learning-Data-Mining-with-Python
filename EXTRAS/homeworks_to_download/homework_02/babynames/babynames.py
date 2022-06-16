58d45e817aff074541747ed13d08d52c246c401c
michalhem <michalhem@pm.me>
2022-06-16 19:14:31 -0300

    commit

--- EXTRAS/homeworks_to_download/homework_02/babynames/babynames.py
+++ EXTRAS/homeworks_to_download/homework_02/babynames/babynames.py
@@ -3,8 +3,6 @@
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
 
 import sys
 import re
